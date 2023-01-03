29ee45d4eacbf6408f68be7a8feaf79ba902a19b
Erick Ferreira de Oliveira <63250462+erick375@users.noreply.github.com>
2023-01-03 12:25:19 +0000

    Pagina para os jogadores da minha campanha resgatarem códigos conseguidos de diversas formas

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function cod() {
+  var mod = document.getElementById("codi").value;
+  var res = document.getElementById("reco");
+  if (mod == 'AVENTURA' || mod == 'ESTRELA'|| mod == 'GOLAGOG' || mod == 'URTALA') {
+    res.style.color= "#348ddf"
+    res.innerHTML = `Você ganhou 5 Pontos Estelares`
+  } else if (mod == 'dd') {
+      res.innerHTML = `p`
+  } else if (mod == 'gg') {
+      res.style.color= "#348ddf"
+      res.innerHTML = `ggg`
+  }
+  else {
+    res.style.color= "red"
+    res.innerHTML = `Insira um código válido`
+  }
+}
+/*var r = mod*/
