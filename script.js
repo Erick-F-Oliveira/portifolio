020cfd99952c100be96630c9d40639b07dffc214
Erick Ferreira de Oliveira <63250462+erick375@users.noreply.github.com>
2023-01-14 02:31:19 +0000

    Adicionado cards de perssonagem(beta) Mudança de cores

--- script.js
+++ script.js
@@ -3,20 +3,22 @@
 function cod() {
   var mod = document.getElementById("codi").value;
   var res = document.getElementById("reco");
+  
   if (mod == 'AVENTURA' || mod == 'ESTRELA'|| mod == 'GOLAGOG' || mod == 'URTALA') {
     res.style.fontFamily= "Tormenta"
     res.style.color= "#348ddf"
     res.innerHTML = `Você ganhou 5 Pontos Estelares`
-  } else if (mod == 'dd') {
+     } else if (mod == 'dd') {
+      res.style.fontFamily= "Tormenta"
       res.innerHTML = `p`
   } else if (mod == 'gg') {
-    
+      res.style.fontFamily= "Tormenta"
       res.style.color= "#348ddf"
       res.innerHTML = `ggg`
   }
   else {
+    res.style.fontFamily= "Tormenta"
     res.style.color= "red"
     res.innerHTML = `Insira um código válido`
   }
 }
-/*var r = mod*/
---
+++ scriptNav.js
@@ -0,0 +1,8 @@
+function openImg() {
+  document.getElementById("barda").style.width = "100%";
+  console.log(`ok`);
+}
+
+function closeImg() {
+  document.getElementById("kali").style.width = "0";
+}
