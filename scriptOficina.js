44d7bbb5f2ce3fdb1abc003dad05068d3cad119c
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2023-01-18 15:58:18 +0000

    Remoção de arquivos desnecessários Edição de quantidade de Modificações e Encantos

--- scriptOficina.js
+++ scriptOficina.js
@@ -2,15 +2,14 @@ function cd(){
   var tipo = document.getElementById('tipo');
   var mod = document.getElementById('mod');
   var enc = document.getElementById('enc');
-  var red = document.getElementById('redu');
+  
 
   var t = Number(tipo.value)
   var m = Number(mod.value)
   var e = Number(enc.value)
-  var rt = Number(red.value)
   var r =''
   var res = document.getElementById('dificuldade')
-  r = ((m + e) * 5 + t+rt)
+  r = ((m + e) * 5 + t)
   res.innerHTML = ` ${r}`
   
   }
